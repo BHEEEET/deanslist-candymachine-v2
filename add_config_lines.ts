03f8d5689bc1da9203a29779a221d11bfe27f2e5
krkbheet@gmail.com <krkbheet@gmail.com>
2025-01-19 13:21:19 +0100

    add config script, add mint

---
+++ add_config_lines.ts
@@ -0,0 +1,53 @@
+import { addConfigLines, fetchCandyMachine } from '@metaplex-foundation/mpl-core-candy-machine';
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import { readFile } from 'fs/promises';
+import { createSignerFromKeypair, publicKey, signerIdentity, some } from '@metaplex-foundation/umi';
+
+const RPC_ENDPOINT = 'https://api.devnet.solana.com';
+const WALLET_PATH = 'C:\\Users\\artis\\.config\\solana\\id.json'; // Update with your Windows username
+
+const candyMachine = publicKey('DRKFiJNJeg4FC8hYntWcbnDQi1gJdY3vHSjPL54gs28D');
+const metadatafile = './uploaded_metadata.json';
+
+
+const addDataToCandy = async () => {
+    try {
+        // Load the default Solana wallet
+        const walletFile = await readFile(WALLET_PATH, 'utf8');
+        const wallet = JSON.parse(walletFile);
+
+        // Initialize Umi with the chosen RPC endpoint
+        const umi = createUmi(RPC_ENDPOINT);
+        const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+        const signer = createSignerFromKeypair(umi, keypair);
+        umi.use(signerIdentity(signer));
+
+        const candyMachineData = await fetchCandyMachine(umi, candyMachine)
+
+        // Read and parse metadata file
+        const fileContent = await readFile(metadatafile, 'utf-8');
+        let configLines = JSON.parse(fileContent);
+        
+
+        // Ensure the parsed content matches the expected type
+        if (!Array.isArray(configLines) || !configLines.every(line => line.name && line.uri)) {
+            throw new Error('Invalid metadata format. Each item must have "name" and "uri".');
+        }
+
+        // Remove the last item from the configLines array
+        configLines.pop();
+
+        console.log(configLines)
+
+        // Add the config lines
+        await addConfigLines(umi, {
+            candyMachine: candyMachine,
+            index: 0,
+            configLines: configLines,
+        }).sendAndConfirm(umi);
+    } catch (e) {
+        console.error('Failed to add NFTs: ', e);
+    }
+};
+
+addDataToCandy();
--- mint_nft.ts
+++ mint_nft.ts
@@ -1,5 +1,49 @@
 
+import { fetchCandyMachine, mintAssetFromCandyMachine, mintV1, mplCandyMachine  } from "@metaplex-foundation/mpl-core-candy-machine";
+import { setComputeUnitLimit } from "@metaplex-foundation/mpl-toolbox";
+import { readFile } from 'fs/promises';
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import { generateSigner, keypairIdentity } from "@metaplex-foundation/umi";
+import { createSignerFromKeypair, publicKey, signerIdentity, some } from '@metaplex-foundation/umi';
 
+const RPC_ENDPOINT = 'https://api.devnet.solana.com';
+const WALLET_PATH = 'C:\\Users\\artis\\.config\\solana\\id.json'; // Update with your Windows username
 
+const candyMachineId = publicKey("DRKFiJNJeg4FC8hYntWcbnDQi1gJdY3vHSjPL54gs28D");
+const coreCollection = publicKey("6J9xzHz9QddxyaJ6VwzXHuXSvNoSokx2GfEVo92QCqCS");
 
-const candyMachine = "FPKjQ8qsxJGKtRZ2Ak1zuoHo2M97BViKQ7DCpLKF5bKX"
+const mint = async () => {
+    try {
+        // Load the default Solana wallet
+        const walletFile = await readFile(WALLET_PATH, 'utf8');
+        const wallet = JSON.parse(walletFile);
+
+        // Initialize Umi with the chosen RPC endpoint
+        const umi = createUmi(RPC_ENDPOINT);
+        const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+        const signer = createSignerFromKeypair(umi, keypair);
+        umi.use(signerIdentity(signer));
+        umi.use(mplCandyMachine())
+
+        const candyMachineData = await fetchCandyMachine(umi, candyMachineId)
+        console.log(candyMachineData)
+
+        const asset = generateSigner(umi);
+
+        await mintAssetFromCandyMachine(umi, {
+            candyMachine: candyMachineId,
+            mintAuthority: umi.identity,
+            assetOwner: umi.identity.publicKey,
+            asset,
+            collection: coreCollection,
+        }).sendAndConfirm(umi);
+
+        console.log('NFT Address:', asset);
+        console.log('View on Solscan:', `https://solscan.io/token/${asset}?cluster=devnet`);
+
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+mint()
